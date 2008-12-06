5bb9b3bb8446f706c3ca7373449dd27aa8d5a226
William Travis Holton <wtholton@googlemail.com>
2008-12-06 15:53:35 +0100

    string input handle
    Reading strings from an input handle now

--- t/lib/Datatype/String.pm
+++ t/lib/Datatype/String.pm
@@ -22,11 +22,40 @@ sub t010_read_hessian_string : Test(1) {    #{{{
 }    #}}}
 
 sub t020_read_handle_terminal_chunk : Test(1) {    #{{{
-    my $hessian_string = "\x{00}\x{05}hello";
-    open my $ih, "<", \$hessian_string 
-    or croak "Could not read from string.";
-    my $string = read_string_handle_chunk($ih);
+    my $hessian_string = "S\x{00}\x{05}hello";
+    open my $ih, "<", \$hessian_string
+      or croak "Could not read from string.";
+    my $first_bit;
+    read $ih, $first_bit, 1;
+    my $string = read_string_handle_chunk( $first_bit, $ih );
     is( $string, 'hello', "Successfully read simple string handle chunk" );
+}    #}}}
+
+sub t021_read_simple_string : Test(1) {    #{{{
+    my $hessian_string = "\x05hello";
+    open my $ih, "<", \$hessian_string or croak "Could not read from string.";
+    my $first_bit;
+    read $ih, $first_bit, 1;
+    my $string = read_string_handle_chunk( $first_bit, $ih );
+    is( $string, 'hello', "Successfully read simple string handle chunk" );
+
+}    #}}}
+
+sub t022_read_two_chunk_string : Test(1) {    #{{{
+    my $hessian_string = "\x52\x00\x07hello, \x05wörld";
+    open my $ih, "<", \$hessian_string or croak "Could not read from string.";
+    my $first_bit;
+    my $raw_string;
+    while ( read $ih, $first_bit, 1 ) {
+        $raw_string .= read_string_handle_chunk( $first_bit, $ih );
+    }
+    my $string = $raw_string;
+    is(
+        $string,
+        "hello, w\xf6rld",
+        "Successfully read two chunk strink from string handle"
+    );
+
 }    #}}}
 
 "one, but we're not the same";
