e0e5f8a40adb1e55f858ef3d792870d0a81970da
William Travis Holton <wtholton@googlemail.com>
2008-12-14 22:13:05 +0100

    typed lists
    working on various types of typed lists

--- lib/Hessian/Translator/List.pm
+++ lib/Hessian/Translator/List.pm
@@ -36,33 +36,13 @@ sub read_complex_datastructure : Export(:input_handle) {    #{{{
     my ( $first_bit, $input_handle ) = @_;
     my ($datastructure);
     switch ($first_bit) {
-        case /\x55/ {    # variable length list
-            my $type = read_list_type($input_handle);
-        }
-        case /\x56/ {    # fixed length list
-            my $type = read_list_type($input_handle);
-            my $length;
-            read $input_handle, $length, 1;
-            my $array_length =
-              read_integer_handle_chunk( $length, $input_handle );
-            $datastructure = [];
-            foreach my $index ( 1 .. $array_length ) {
-                my $element;
-                read $input_handle, $element, 1;
-                my $integer =
-                  read_integer_handle_chunk( $element, $input_handle );
-                push @{$datastructure}, $integer;
-
-            }
-
+        case /[\x55\x56\x70-\x77]/ {                        # typed lists
+            $datastructure = read_typed_list( $first_bit, $input_handle );
         }
         case /\x57/ {    # variable length untyped list
 
         }
         case /\x58/ {    # fixed length untyped list
-
-        }
-        case /[\x70-\x77]/ {    # fixed length typed list
 
         }
         case /[\x78-\x7f]/ {    # fixed length untyped list
@@ -74,12 +54,56 @@ sub read_complex_datastructure : Export(:input_handle) {    #{{{
 
 }    #}}}
 
+sub read_typed_list {    #{{{
+    my ( $first_bit, $input_handle ) = @_;
+    my $type = read_list_type($input_handle);
+    my $array_length;
+    if ( $first_bit =~ /\x56/ ) { # read array length
+        my $length;
+        read $input_handle, $length, 1;
+        $array_length = read_integer_handle_chunk( $length, $input_handle );
+    }
+    elsif (  $first_bit =~ /[\x70-\x77]/) {
+        my $hex_bit = unpack 'C*', $first_bit;
+         $array_length = $hex_bit - 0x70;
+    }
+
+    my $datastructure = [];
+    my $index = 0;
+  LISTLOOP:
+    {
+        last LISTLOOP if ( $array_length and ( $index == $array_length ) );
+        my $element = read_typed_list_element( $type, $input_handle );
+        last LISTLOOP if $first_bit =~ /\x55/ && $element eq 'Z';
+
+        push @{$datastructure}, $element;
+        $index++;
+        redo LISTLOOP;
+    }
+    return $datastructure;
+}    #}}}
+
+sub read_typed_list_element {    #{{{
+    my ( $type, $input_handle ) = @_;
+    my $element;
+    my $first_bit;
+    read $input_handle, $first_bit, 1;
+    return $first_bit if $first_bit eq 'Z';
+    switch ($type) {
+        case /int/ {
+            binmode( $input_handle, 'bytes' );
+            $element = read_integer_handle_chunk( $first_bit, $input_handle );
+        }
+
+    }
+    return $element;
+}    #}}}
+
 sub read_list_type {    #{{{
     my $input_handle = shift;
     my $type_length;
     read $input_handle, $type_length, 1;
     my $type = read_string_handle_chunk( $type_length, $input_handle );
-    print "Type $type\n";
     binmode( $input_handle, 'bytes' );
     return $type;
 }    #}}}
--- t/lib/Datatype/List.pm
+++ t/lib/Datatype/List.pm
@@ -11,15 +11,44 @@ use Test::Deep;
 
 use Hessian::Translator::List qw/:input_handle/;
 
-sub t010_read_fixed_length_untyped : Test(1) {    #{{{
+sub t010_read_fixed_length_typed : Test(1) {    #{{{
     my $self         = shift;
     my $hessian_data = "V\x04[int\x92\x90\x91";
     my $ih           = $self->get_string_file_input_handle($hessian_data);
     my $first_bit;
     read $ih, $first_bit, 1;
     my $datastructure = read_complex_datastructure( $first_bit, $ih );
-    cmp_deeply($datastructure, [0,1], "Received expected datastructure.");
+    cmp_deeply( $datastructure, [ 0, 1 ], "Received expected datastructure." );
 }    #}}}
+
+sub t011_read_variable_length_typed : Test(1) {    #{{{
+    my $self         = shift;
+    my $hessian_data = "\x55\x04[int\x90\x91\xd7\xff\xffZ";
+    my $ih           = $self->get_string_file_input_handle($hessian_data);
+    my $first_bit;
+    read $ih, $first_bit, 1;
+    my $datastructure = read_complex_datastructure( $first_bit, $ih );
+    cmp_deeply(
+        $datastructure,
+        [ 0, 1, 262143 ],
+        "Received expected datastructure."
+    );
+}    #}}}
+
+sub  t012_read_fixed_length_type : Test(1){ #{{{
+    my $self = shift;
+    my $hessian_data = "\x73\x04[int\x90\x91\xd7\xff\xff";
+    my $ih           = $self->get_string_file_input_handle($hessian_data);
+    my $first_bit;
+    read $ih, $first_bit, 1;
+    my $datastructure = read_complex_datastructure( $first_bit, $ih );
+    cmp_deeply(
+        $datastructure,
+        [ 0, 1, 262143 ],
+        "Received expected datastructure."
+    );
+} #}}}
+
 
 "one, but we're not the same";
 
