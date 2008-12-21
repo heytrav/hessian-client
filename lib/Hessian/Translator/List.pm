3481c2e93798060b6b72d5feae572f4a9dcc4d71
William Travis Holton <wtholton@googlemail.com>
2008-12-21 18:05:50 +0100

    list deserialization
    more tests of atomic and composite datataype parsing

--- lib/Hessian/Translator/List.pm
+++ lib/Hessian/Translator/List.pm
@@ -39,16 +39,9 @@ sub read_complex_datastructure : Export(:input_handle) {    #{{{
         case /[\x55\x56\x70-\x77]/ {                        # typed lists
             $datastructure = read_typed_list( $first_bit, $input_handle );
         }
-        case /\x57/ {    # variable length untyped list
-
+        case /[\x57\x58\x78-\x7f]/ {                        # untyped lists
+            $datastructure = read_untyped_list( $first_bit, $input_handle );
         }
-        case /\x58/ {    # fixed length untyped list
-
-        }
-        case /[\x78-\x7f]/ {    # fixed length untyped list
-
-        }
-
     }
     return $datastructure;
 
@@ -57,24 +50,56 @@ sub read_complex_datastructure : Export(:input_handle) {    #{{{
 sub read_typed_list {    #{{{
     my ( $first_bit, $input_handle ) = @_;
     my $type = read_list_type($input_handle);
-    my $array_length;
-    if ( $first_bit =~ /\x56/ ) { # read array length
-        my $length;
-        read $input_handle, $length, 1;
-        $array_length = read_integer_handle_chunk( $length, $input_handle );
-    }
-    elsif (  $first_bit =~ /[\x70-\x77]/) {
-        my $hex_bit = unpack 'C*', $first_bit;
-         $array_length = $hex_bit - 0x70;
-    }
+    my $array_length = read_list_length( $first_bit, $input_handle );
 
     my $datastructure = [];
-    my $index = 0;
+    my $index         = 0;
   LISTLOOP:
     {
         last LISTLOOP if ( $array_length and ( $index == $array_length ) );
         my $element = read_typed_list_element( $type, $input_handle );
         last LISTLOOP if $first_bit =~ /\x55/ && $element eq 'Z';
+
+        push @{$datastructure}, $element;
+        $index++;
+        redo LISTLOOP;
+    }
+    return $datastructure;
+}    #}}}
+
+sub read_list_length {    #{{{
+    my ( $first_bit, $input_handle ) = @_;
+
+    my $array_length;
+    if ( $first_bit =~ /[\x56\x58]/ ) {    # read array length
+        my $length;
+        read $input_handle, $length, 1;
+        $array_length = read_integer_handle_chunk( $length, $input_handle );
+    }
+    elsif ( $first_bit =~ /[\x70-\x77]/ ) {
+        my $hex_bit = unpack 'C*', $first_bit;
+        $array_length = $hex_bit - 0x70;
+    }
+    elsif ( $first_bit =~ /[\x78-\x7f]/ ) {
+        my $hex_bit = unpack 'C*', $first_bit;
+        $array_length = $hex_bit - 0x78;
+    }
+    print "Read list length = $array_length\n";
+    return $array_length;
+}    #}}}
+
+sub read_untyped_list {    #{{{
+    my ( $first_bit, $input_handle ) = @_;
+    my $array_length = read_list_length( $first_bit, $input_handle );
+
+    my $datastructure = [];
+    my $index         = 0;
+  LISTLOOP:
+    {
+        last if $index == 10;
+        last LISTLOOP if ( $array_length and ( $index == $array_length ) );
+        my $element = read_untyped_list_element($input_handle);
+        last LISTLOOP if $first_bit =~ /\x57/ && $element eq 'Z';
 
         push @{$datastructure}, $element;
         $index++;
@@ -97,6 +122,40 @@ sub read_typed_list_element {    #{{{
 
     }
     return $element;
+}    #}}}
+
+sub read_untyped_list_element {    #{{{
+    my $input_handle = shift;
+    my $element;
+    my $first_bit;
+    read $input_handle, $first_bit, 1;
+    return $first_bit if $first_bit eq 'Z';
+    switch ($first_bit) {
+        case /[\x49\x80-\xbf\xc0-\xcf\xd0-\xd7]/ {
+            $element = read_integer_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x4c\xd8-\xef\xf0-\xff\x38-\x3f\x59]/ {
+            $element = read_long_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x44\x5b-\x5f]/ {
+            $element = read_double_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x4a\x4b]/ {
+            $element = read_date_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x52\x53\x00-\x1f\x30-\x33]/ {
+            $element = read_string_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x41\x42\x20-\x2f\x34-\x37]/ {
+            $element = read_binary_handle_chunk( $first_bit, $input_handle );
+        }
+        case /[\x55-\x58\x70-\x7f]/ {
+            $element = read_complex_datastructure( $first_bit, $input_handle );
+        }
+
+    }
+    return $element;
+
 }    #}}}
 
 sub read_list_type {    #{{{
--- t/lib/Datatype/List.pm
+++ t/lib/Datatype/List.pm
@@ -8,7 +8,7 @@ use base 'Datatype';
 
 use Test::More;
 use Test::Deep;
-
+use YAML;
 use Hessian::Translator::List qw/:input_handle/;
 
 sub t010_read_fixed_length_typed : Test(1) {    #{{{
@@ -48,6 +48,22 @@ sub  t012_read_fixed_length_type : Test(1){ #{{{
         "Received expected datastructure."
     );
 } #}}}
+
+sub  t013_read_fixed_length_untyped : Test(1) { #{{{
+    my $self = shift;
+    my $hessian_data = "\x57\x90\x91\xd7\xff\xff\x52\x00\x07hello, \x05worldZ";
+    my $ih           = $self->get_string_file_input_handle($hessian_data);
+    my $first_bit;
+    read $ih, $first_bit, 1;
+
+    my $datastructure = read_complex_datastructure($first_bit, $ih);
+    cmp_deeply(
+    $datastructure,
+        [ 0, 1, 262143,'hello, ', 'world' ],
+        "Received expected datastructure."
+    );
+} #}}}
+
 
 
 "one, but we're not the same";
