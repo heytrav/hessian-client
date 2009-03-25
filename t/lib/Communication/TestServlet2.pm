4142674160b6c18d5d7aa27f01c312b6ae32e243
William Travis Holton <wtholton@googlemail.com>
2009-03-25 22:46:13 +0100

    basic tests with TestServlet2

--- t/lib/Communication/TestServlet2.pm
+++ t/lib/Communication/TestServlet2.pm
@@ -5,46 +5,111 @@ use warnings;
 
 use base 'Communication';
 
-
 use Test::More;
 use Test::Deep;
 use Test::Exception;
 
 use Hessian::Client;
+use YAML;
 
-my $test_service = 'http://hessian.caucho.com/test/test2'; 
+my $test_service = 'http://hessian.caucho.com/test/test2';
 
-sub prep01_check_webservice : Test(startup)  { #{{{
-    my $self = shift;
-    my $client = Hessian::Client->new(  { 
-        version => 2, service => $test_service
-        });
-       eval {
-           
-       my $result = $client->methodNull(); 
-       };
-       if (my $e = $@) {
+sub prep01_check_webservice : Test(startup) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    eval {
+
+        my $result = $client->methodNull();
+    };
+    if ( my $e = $@ ) {
         $self->SKIP_ALL("Problem connecting to test service.");
-       }
+    }
 
-} #}}}
+}    #}}}
+
+sub test_reply_int_0 : Test(1) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $result = $client->replyInt_0();
+    is( $result->{reply_data}, 0 );
+}    #}}}
+
+sub test_reply_int_47 : Test(1) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $result = $client->replyInt_47();
+    is( $result->{reply_data}, 47 );
+}    #}}}
+
+sub test_reply_int_mx800 : Test(1) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $function = "replyInt_m0x800";
+    my $result = $client->$function();
+    is( $result->{reply_data}, -0x800 );
+}    #}}}
 
 
-sub t030_reply_object_16 : Test(1) { #{{{
-    my $self = shift;
-        my $hessian_client = Hessian::Client->new(
-            {
-                version => 2,
-                service => $test_service
-            }
-        );
-        my $result = $hessian_client->replyObject_16();
+sub test_reply_int_mOx80000000 : Test(1) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $function = "replyLong_m0x80000000";
+    my $result = $client->$function();
+    is( $result, -0x80000000);
+}    #}}}
+
+sub test_reply_int_m17 : Test(1) {    #{{{
+    my $self   = shift;
+    my $client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $result = $client->replyInt_m17();
+    is( $result->{reply_data}, -17 );
+}    #}}}
+
+sub t030_reply_object_16 : Test(1) {    #{{{
+    my $self           = shift;
+    my $hessian_client = Hessian::Client->new(
+        {
+            version => 2,
+            service => $test_service
+        }
+    );
+    my $result = $hessian_client->replyObject_16();
     cmp_deeply(
         $result,
-        { hessian_version => '2.0',reply_data => array_each(ignore())},
+        { hessian_version => '2.0', reply_data => array_each( ignore() ) },
         "Received expected header from service."
     );
-} #}}}
+}    #}}}
 
 "one, but we're not the same";
 
